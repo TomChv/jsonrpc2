2f3dc7b969ed5d138a7e2d45ba4940e5eba82a18
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-03-05 19:57:55 +0100

    Improve parse request and tests
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- common/error.go
+++ common/error.go
@@ -5,13 +5,13 @@ import "fmt"
 // RpcError is a JSON-RPC 2.0 Object that represent an error from the server
 type RpcError struct {
 	// Code is a number that indicate the error type that occurred
-	Code int64
+	Code int64 `json:"code"`
 
 	// Message is a short description of the error
-	Message string
+	Message string `json:"message"`
 
 	// Data contains additional information about the error
-	Data interface{}
+	Data interface{} `json:"data,omitempty"`
 }
 
 func (e *RpcError) Error() string {
--- server/parse.go
+++ server/parse.go
@@ -5,19 +5,32 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 
 	"github.com/TomChv/jsonrpc2/common"
 )
 
 var (
-	ErrInvalidJsonVersion = errors.New("invalid JSON RPC version")
-	ErrNoMethodProvided   = errors.New("no method provided")
+	ErrInvalidJsonVersion    = errors.New("invalid JSON RPC version")
+	ErrNoMethodProvided      = errors.New("no method provided")
+	ErrInvalidHTTPMethod     = errors.New("http method should be POST")
+	ErrInvalidPathRequest    = errors.New("http request should target /")
+	ErrInvalidBody           = errors.New("http request invalid body")
+	ErrInvalidIdentifierType = errors.New("http request invalid id type")
 )
 
 // parseRequest transform a HTTP request into a valid Request object.
 //
 // If the request does not match the specification, it returns a RpcError.
 func parseRequest(r *http.Request) (*Request, *RpcError) {
+	if r.Method != http.MethodPost {
+		return nil, InvalidRequestError(ErrInvalidHTTPMethod.Error())
+	}
+
+	if r.URL.Path != "/" {
+		return nil, InvalidRequestError(ErrInvalidPathRequest.Error())
+	}
+
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		return nil, ParsingError(err.Error())
@@ -25,17 +38,32 @@ func parseRequest(r *http.Request) (*Request, *RpcError) {
 
 	var req Request
 	if err := json.Unmarshal(body, &req); err != nil {
-		return nil, InvalidRequestError(err.Error())
+		return nil, InvalidRequestError(ErrInvalidBody)
 	}
 
-	if req.JsonRpc == nil || *req.JsonRpc != common.JSON_RPC_VERSION {
+	if req.JsonRpc != common.JSON_RPC_VERSION {
 		return nil, InvalidRequestError(ErrInvalidJsonVersion.Error())
 	}
 
-	if req.Method == nil {
+	if req.Method == "" {
 		return nil, InvalidRequestError(ErrNoMethodProvided.Error())
 	}
 
-	// TODO parse ID
+	if req.ID != nil {
+		switch reflect.TypeOf(req.ID).String() {
+		case "string":
+			break
+		case "float64":
+			// Verify if it's an integer or a float
+			if req.ID != float64(int(req.ID.(float64))) {
+				return nil, InvalidRequestError(ErrInvalidIdentifierType.Error())
+			}
+			req.SetID(int(req.ID.(float64)))
+			break
+		default:
+			return nil, InvalidRequestError(ErrInvalidIdentifierType.Error())
+		}
+	}
+
 	return &req, nil
 }

1352754969259297eaef7a53004bf76eca7fd8ee
Vasek - Tom C <tom.chauveau@epitech.eu>
2022-03-08 00:37:11 +0100

    Add parseMethod function to correctly parse which rpc is called
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- server/parse.go
+++ server/parse.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"strings"
 
 	"github.com/TomChv/jsonrpc2/common"
 )
@@ -17,6 +18,7 @@ var (
 	ErrInvalidPathRequest    = errors.New("http request should target /")
 	ErrInvalidBody           = errors.New("http request invalid body")
 	ErrInvalidIdentifierType = errors.New("http request invalid id type")
+	ErrInvalidMethodFormat   = errors.New("invalid method format")
 )
 
 // parseRequest transform a HTTP request into a valid Request object.
@@ -67,4 +69,39 @@ func parseRequest(r *http.Request) (*Request, *RpcError) {
 	}
 
 	return &req, nil
+}
+
+// procedure is a container around method easily parse which rpc must be called
+type procedure struct {
+	Service string
+	Method  string
+}
+
+// parseMethod transform a method string into an object procedure
+// If there is no _, it returns the method with no service "" (e.g "sum")
+// If there is a _, it split the string and return the first part as a service
+// and the second part as a method (e.g "eth_getBalance")
+// Otherwise, it returns an error
+func parseMethod(method string) (*procedure, *RpcError) {
+	toPascalCase := func(str string) string {
+		if str == "" {
+			return ""
+		}
+		return strings.ToUpper(string(str[0])) + str[1:]
+	}
+
+	switch strings.Count(method, "_") {
+	case 0:
+		return &procedure{
+			Method: toPascalCase(method),
+		}, nil
+	case 1:
+		p := strings.Split(method, "_")
+		return &procedure{
+			Service: p[0],
+			Method:  toPascalCase(p[1]),
+		}, nil
+	default:
+		return nil, InvalidRequestError(ErrInvalidMethodFormat.Error())
+	}
 }
